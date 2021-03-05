7705efd5720e1733e2d2565b2aebf7af105f590e
nobmurakami <mhpdm541@gmail.com>
2021-03-05 19:48:17 +0900

    Update pincerattack_s2.rb

--- Ruby/paiza/a_rank_level_up_problems/pincerattack_s2.rb
+++ Ruby/paiza/a_rank_level_up_problems/pincerattack_s2.rb
@@ -24,16 +24,14 @@ def check_row(y, x, map)
 end
 
 # 現在地y, xから上下を順番に探索するメソッド
+# 基本的にcheck_rowと同じ
 def check_column(y, x, map)
-  # lrで探索の方向を切り替える
   [-1, 1].each do |lr|
     i = 0
     while true
       i += 1
-      # マップの外に出たら探索を打ち切る
       if y + i * lr < 0 || y + i * lr >= $h
         break
-      # 自分の石が見つかったら、間のマスを自分の石で埋めて、探索を打ち切る
       elsif map[y + i * lr][x] == "*"
         i.times do |j|
           map[y + j * lr][x] = "*"
