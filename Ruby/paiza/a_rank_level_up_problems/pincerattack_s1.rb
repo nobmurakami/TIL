0cb5c56b712be2c000bcfd376227ee6bec78d91b
nobmurakami <mhpdm541@gmail.com>
2021-03-05 20:20:37 +0900

    Update pincerattack_s1.rb

--- Ruby/paiza/a_rank_level_up_problems/pincerattack_s1.rb
+++ Ruby/paiza/a_rank_level_up_problems/pincerattack_s1.rb
@@ -2,11 +2,12 @@
 h, w, sy, sx = gets.split.map {|i| i.to_i}
 map = h.times.map { "." * w }
 
+# 石を置く
+map[sy][sx] = "!"
+
 h.times do |y|
   w.times do |x|
-    if y == sy && x == sx
-      map[y][x] = "!"
-    elsif y == sy || x == sx
+    if y == sy || x == sx
       map[y][x] = "*"
     end
   end
